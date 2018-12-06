59bde39281932a8c29b3a5d24d12d3c40bb6010b
Peter M C Harrison <pmc.harrison@gmail.com>
2018-12-06 16:09:29 +0000

    Polishing up

--- R/pc-harmonicity.R
+++ R/pc-harmonicity.R
@@ -1,11 +1,11 @@
 #' @export
 pc_harmonicity <- function(x,
                            method = "kl",
-                           type = "pc",
                            num_harmonics = 12,
                            rho = 0.75,
                            sigma = 6.83,
-                           array_dim = 1200) {
+                           array_dim = 1200,
+                           ...) {
   UseMethod("pc_harmonicity")
 }
 
@@ -28,7 +28,8 @@ pc_harmonicity.pc_set <- function(x,
                                 num_harmonics = num_harmonics,
                                 rho = rho,
                                 sigma = sigma,
-                                array_dim = array_dim)
+                                array_dim = array_dim,
+                                ...)
   pc_harmonicity(x,
                  method = method,
                  num_harmonics = num_harmonics,
@@ -41,14 +42,14 @@ pc_harmonicity.pc_smooth_spectrum <- function(x,
                                               method = "kl",
                                               num_harmonics = 12,
                                               rho = 0.75,
-                                              sigma = 6.83) {
+                                              sigma = 6.83,
+                                              ...) {
   checkmate::qassert(method, "S1")
 
   y <- sweep_harmonic_template(x,
                                num_harmonics = num_harmonics,
                                rho = rho,
-                               sigma = sigma,
-                               array_dim = array_dim)
+                               sigma = sigma)
   if (method == "kl") {
     kl_div_from_uniform(y)
   } else if (method == "peak") {
