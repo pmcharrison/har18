f68288e6193759df7b751907a75c324a61525616
Peter M C Harrison <pmc.harrison@gmail.com>
2018-12-06 12:26:24 +0000

    Renaming to pc_harmonicity

---
+++ R/pc-harmonicity.R
@@ -0,0 +1,57 @@
+#' @export
+pc_harmonicity <- function(x,
+                           method = "kl",
+                           type = "pc",
+                           num_harmonics = 12,
+                           rho = 0.75,
+                           sigma = 6.83,
+                           array_dim = 1200) {
+  UseMethod("pc_harmonicity")
+}
+
+#' @export
+pc_harmonicity.default <- function(x, ...) {
+  pc_harmonicity(pc_set(x), ...)
+}
+
+#' @export
+pc_harmonicity.pc_set <- function(x,
+                                  method = "kl",
+                                  num_harmonics = 12,
+                                  rho = 0.75,
+                                  sigma = 6.83,
+                                  array_dim = 1200,
+                                  ...) {
+  checkmate::qassert(method, "S1")
+
+  x <- hrep::pc_smooth_spectrum(x,
+                                num_harmonics = num_harmonics,
+                                rho = rho,
+                                sigma = sigma,
+                                array_dim = array_dim)
+  pc_harmonicity(x,
+                 method = method,
+                 num_harmonics = num_harmonics,
+                 rho = rho,
+                 sigma = sigma)
+}
+
+#' @export
+pc_harmonicity.pc_smooth_spectrum <- function(x,
+                                              method = "kl",
+                                              num_harmonics = 12,
+                                              rho = 0.75,
+                                              sigma = 6.83) {
+  checkmate::qassert(method, "S1")
+
+  y <- sweep_harmonic_template(x,
+                               num_harmonics = num_harmonics,
+                               rho = rho,
+                               sigma = sigma,
+                               array_dim = array_dim)
+  if (method == "kl") {
+    kl_div_from_uniform(y)
+  } else if (method == "peak") {
+    peak(y)
+  } else stop("unrecognised method")
+}
